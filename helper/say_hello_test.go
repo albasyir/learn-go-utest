85e29d037875329c34e3dc6b97c0f1b93607b206
Abdul Aziz Al Basyir <abdulazizalbasyir119@gmail.com>
2023-08-27 18:18:19 +0700

    banchmarking go

--- helper/say_hello_test.go
+++ helper/say_hello_test.go
@@ -17,6 +17,55 @@ func TestMain(m *testing.M) {
 	fmt.Println("after test")
 }
 
+func BenchmarkSayHelloAziz(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		SayHello("Aziz")
+	}
+}
+
+func BenchmarkSayHelloSubBenchmark(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		SayHello("Aziz")
+	}
+
+	b.Run("Aziz", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			SayHello("Aziz")
+		}
+	})
+
+	b.Run("Albasyir", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			SayHello("Albasyir")
+		}
+	})
+}
+
+func BenchmarkTableImplementation(b *testing.B) {
+	tests := []struct {
+		x int
+		y int
+	}{
+		{1, 1},
+		{2, 2},
+		{3, 3},
+	}
+
+	for _, test := range tests {
+		b.Run(fmt.Sprintf("%d x %d = %d", test.x, test.y, test.x*test.y), func(t *testing.B) {
+			for i := 0; i < t.N; i++ {
+				DimentionCalculation(test.x, test.y)
+			}
+		})
+	}
+}
+
+func BenchmarkSayHelloMoreData(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		SayHello("ThisWithMoreDataSoThisWillBeHeavyThanAzizString")
+	}
+}
+
 func TestSayHello(t *testing.T) {
 	assert.Equal(t, "Hello, Aziz!", SayHello("Aziz"), "Result must be 'Hello, Aziz!'")
 }
--- note.md
+++ note.md
@@ -1,11 +1,49 @@
+# U Test on Golang
+
+describe how to run unit test on golang
+
+## Unit Test
+
+- must be declared by func `Test..` prefix, ex. `TestSayHello`
+
 ### Run utest
 
 ```bash
 go test -v ./...
 ```
 
-### Run one folder
+### Run one package
 
 ```bash
 go test -v ./helper/
 ```
+
+`./helper/` is package name
+
+### mocking and assert
+
+this example using `testify` package, see on project how to implement mocking and assert
+
+### Sub Test
+
+can declare subtest that can be use for **table testing implementation**, see example on this project
+
+## Banchmark
+
+### Run banchmark without unit test
+
+```bash
+go test -v -run=UnitTestHasNotExists -bench=.
+```
+
+note that you must define a unit test that has not implemented, like `UnitTestHasNotExists`, this has not implemented by this project
+
+### Run spesific banchmark without unit test
+
+```bash
+go test -v -run=UnitTestHasNotExists -bench=BenchmarkSayHello
+```
+
+### Sub Banchmark
+
+can declare sub banchmark that can be use for **table banchmarking implementation**, see example on this project
